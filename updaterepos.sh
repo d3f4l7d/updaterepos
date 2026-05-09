d1b13cc9974f7fdb4fd0d8e0664c5c5b49f18b35
d3f4l7d <idaten.bekko@gmail.com>
2026-05-09 16:36:59 +0900

    Update updaterepos.sh

--- updaterepos.sh
+++ updaterepos.sh
@@ -8,13 +8,16 @@ echo
 
 rm -rf ~/.config/d3f4l7d/safearch
 rm -rf ~/.config/d3f4l7d/autoinstall
+rm -rf ~/.config/d3f4l7d/archjekyll
 rm -rf ~/.config/d3f4l7d/archfictrac
 rm -rf ~/.config/d3f4l7d/archlightningpose
+rm -rf ~/.config/d3f4l7d/archlightningpose3d
 git clone https://github.com/d3f4l7d/safearch.git ~/.config/d3f4l7d/safearch
 git clone https://github.com/d3f4l7d/autoinstall.git ~/.config/d3f4l7d/autoinstall
+git clone https://github.com/d3f4l7d/archjekyll.git ~/.config/d3f4l7d/archjekyll
 git clone https://github.com/d3f4l7d/archfictrac.git ~/.config/d3f4l7d/archfictrac
 git clone https://github.com/d3f4l7d/archlightningpose.git ~/.config/d3f4l7d/archlightningpose
-
+git clone https://github.com/d3f4l7d/archlightningpose3d.git ~/.config/d3f4l7d/archlightningpose3d
 echo
 echo " _   _ ____  ____    _  _____ _____ ____  _ "
 echo "| | | |  _ \|  _ \  / \|_   _| ____|  _ \| |"
